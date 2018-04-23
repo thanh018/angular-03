26967e19d6087a2eacc8860cac56ab86f17e17da
vanducthanh <thanh.van@sutrixsolutions.com>
2018-04-23 18:34:42 +0700

    initial

---
+++ src/app/fake-movies.ts
@@ -0,0 +1,23 @@
+import { Movie } from '../models/movie';
+export var fakeMovies: Movie[] = [
+    {
+        id: 1,
+        name: 'The Ghost and the Darkness',
+        releaseYear: 1996
+    },
+    {
+        id: 2,
+        name: 'The Blind Side',
+        releaseYear: 2009
+    },
+    {
+        id: 3,
+        name: 'Back to the future',
+        releaseYear: 1985
+    },
+    {
+        id: 4,
+        name: 'The Matrix',
+        releaseYear: 2010
+    }
+];
---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
+import { fakeMovies } from '../fake-movies';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent implements OnInit {
+
+  movie: Movie = {
+    id: 1,
+    name: "Star Wars",
+    releaseYear: 1977
+  }
+  movies = fakeMovies;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  selectedMovie: Movie;
+  onSelect(movie: Movie):void {
+    this.selectedMovie = movie;
+    console.log(JSON.stringify(this.selectedMovie.releaseYear));
+  }
+
+}
